3058d336449cd16b622d8023a1cd4bd13b402670
RogerioGotardo <120601204+RogerioGotardo@users.noreply.github.com>
2023-05-21 22:38:02 -0300

    update

--- aula 12/ex011.js
+++ aula 12/ex011.js
@@ -1,6 +1,9 @@
-var idade = 15
-if (idade < 18) {
-    console.log('Menor de idade')
+var idade = 22
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Não vota')
+} else if(idade < 18 || idade > 65){
+    console.log('Voto opicinal')       
 } else {
-    console.log('Maior de idade')
-    }
+    console.log('Voto obrigatório')
+}
