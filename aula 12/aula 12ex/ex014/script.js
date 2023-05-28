403f8d3882ee79c01a6bade47ad87b54ecac805c
RogerioGotardo <120601204+RogerioGotardo@users.noreply.github.com>
2023-05-28 18:00:32 -0300

    update

--- aula 12/aula 12ex/ex014/script.js
+++ aula 12/aula 12ex/ex014/script.js
@@ -2,21 +2,20 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 18
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >=0 && hora <12) {
+    if (hora >= 0 && hora < 12) {
         //BOM DIA!
         img.src = 'fotomanha.png'
-        document.body.style.background = '#e2cd9f'
+        document.body.style.background = '#4c4420'
     } else if (hora >= 12 &&  hora <= 18) {
         //BOA TARDE!
         img.src = 'fototarde.png'
-        document.body.style.background = '#b9846f'
+        document.body.style.background = '#fed1a9'
     } else {
         //BOA NOITE!
         img.src = ' fotonoite.png'
-        document.body.style.background = '#515154'
+        document.body.style.background = '#3a2c4a'
     }
 
 }
