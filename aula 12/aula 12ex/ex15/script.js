324d487ebbcf09817cd25ffaedee11c1a13f4644
RogerioGotardo <120601204+RogerioGotardo@users.noreply.github.com>
2023-05-28 21:28:28 -0300

    update

--- aula 12/aula 12ex/ex15/script.js
+++ aula 12/aula 12ex/ex15/script.js
@@ -3,8 +3,8 @@ function verificar() {
    var ano = data.getFullYear()
    var fano = document.getElementById('txtano')
    var res = document.querySelector('div#res')
-   if (fano.ariaValueMax.length == 0 || Number(fano.value) > ano) {
-       window.alert('Verifique os dados e tente novamente!')
+   if (fano.Value.length == 0 || Number(fano.value) > ano) {
+       window.alert('[ERRO] Verifique os dados e tente novamente!')
    } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
